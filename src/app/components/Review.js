f772fc119d5c05dbd29bb2ce1798c3a3f9ed0988
shkwahab <wahab209266@gmail.com>
2023-06-04 20:06:51 +0500

    space

--- src/app/components/Review.js
+++ src/app/components/Review.js
@@ -16,28 +16,28 @@ const Review = (props) => {
                 </div>
                 <div className=" ">
                 <div className=" flex lg:flex-row flex-col justify-items-center justify-center">
-                    <div className=' my-1 lg:my-0 sm:my-2 mx-1 sm:mx-2'>
+                    <div className=' my-1 md:my-4 sm:my-2 mx-1 sm:mx-2'>
                         <div className=' '>
                             <img className='w-[250px]' src="/assets/madeinusa.png" alt="Usa" />
                         </div>
                      
                     </div>
                 
-                    <div className='  my-1 lg:my-0 sm:my-2 mx-1 sm:mx-2'>
+                    <div className='  my-1 lg:my-4 sm:my-2 mx-1 sm:mx-2'>
                         <div className=' '>
                             <img className='w-[250px]' src="/assets/bestquality.png" alt="Usa" />
                         </div>
                       
                     </div>
                 
-                    <div className=' mx-1 my-1 sm:my-2 lg:my-0 sm:mx-2'>
+                    <div className=' mx-1 my-1 sm:my-2 lg:my-4 sm:mx-2'>
                         <div className=''>
                             <img className='w-[250px]' src="/assets/freeshipping.png" alt="Usa" />
                         </div>
                         
                     </div>
                 
-                    <div className=' mx-1 my-1 sm:my-2 lg:my-0 sm:mx-2'>
+                    <div className=' mx-1 my-1 sm:my-2 lg:my-4 sm:mx-2'>
                         <div className=''>
                             <img className='w-[250px]' src="/assets/crystal.png" alt="Usa" />
                         </div>
